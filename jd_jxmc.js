e58fdb197b8818467109e4af3dd825e3c69a1a29
676507950 <91559862+676507950@users.noreply.github.com>
2021-11-12 12:06:18 +0800

    Update jd_jxmc.js

--- jd_jxmc.js
+++ jd_jxmc.js
@@ -103,11 +103,11 @@ if ($.isNode()) {
     await pasture();
     await $.wait(2000);
   }
-  $.res = await getAuthorShareCode('https://raw.githubusercontent.com/atyvcn/updateTeam/master/shareCodes/jd/jxmc.json')
+  $.res = await getAuthorShareCode('')
   if (!$.res) {
-    $.http.get({url: 'https://purge.jsdelivr.net/gh/atyvcn/updateTeam@master/shareCodes/jd/jxmc.json'}).then((resp) => {}).catch((e) => console.log('刷新CDN异常', e));
+    $.http.get({url: ''}).then((resp) => {}).catch((e) => console.log('刷新CDN异常', e));
     await $.wait(1000)
-    $.res = await getAuthorShareCode('https://cdn.jsdelivr.net/gh/atyvcn/updateTeam@master/shareCodes/jd/jxmc.json')
+    $.res = await getAuthorShareCode('')
   }
   await shareCodesFormat()
   for (let i = 0; i < cookiesArr.length; i++) {
