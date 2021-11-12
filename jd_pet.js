684b0d2ca53aa517e4c10e7a2d6a7d80e50367f2
676507950 <91559862+676507950@users.noreply.github.com>
2021-11-12 12:47:49 +0800

    Update jd_pet.js

--- jd_pet.js
+++ jd_pet.js
@@ -33,9 +33,9 @@ let cookiesArr = [], cookie = '', jdPetShareArr = [], isBox = false, notify, new
 //下面给出两个账号的填写示例（iOS只支持2个京东账号）
 let shareCodes = [ // IOS本地脚本用户这个列表填入你要助力的好友的shareCode
    //账号一的好友shareCode,不同好友的shareCode中间用@符号隔开
-  'MTEzMzI1MTE4NTAwMDAwMDA1Mzc0NTAwOQ==@MTEzMzI1MTE4NTAwMDAwMDA1Mzc0NTAwOQ==@MTE5MzEwNTEzODAwMDAwMDA1NDIzNjc0Mw==@MTEzMzI1MTE4NDAwMDAwMDA1NTYwMTY3Nw==',
+  '',
   //账号二的好友shareCode,不同好友的shareCode中间用@符号隔开
-  'MTEzMzI1MTE4NTAwMDAwMDA1Mzc0NTAwOQ==@MTEzMzI1MTE4NTAwMDAwMDA1Mzc0NTAwOQ==@MTE5MzEwNTEzODAwMDAwMDA1NDIzNjc0Mw==@MTEzMzI1MTE4NDAwMDAwMDA1NTYwMTY3Nw=='
+  ''
 ]
 let message = '', subTitle = '', option = {};
 let jdNotify = false;//是否关闭通知，false打开通知推送，true关闭通知推送
